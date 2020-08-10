9741307b5bc55e2a80a5e2bf6e57aeee0ec7a8e0
Seraphin <aribi.rachid@hotmail.fr>
2020-08-10 10:55:40 +0200

    third exo

---
+++ ex03.js
@@ -0,0 +1,10 @@
+function checkContent(value){
+    if (value <= 0)
+      return("Enfantillage.");
+    else if(value == 42)
+        return("the answer");
+    else
+        return("je peux aller en boite");
+      
+  }
+  checkContent(0);
