d621890280b646d6771c0cf18f19c0a656ce537c
Seraphin <aribi.rachid@hotmail.fr>
2020-08-10 10:51:10 +0200

    second exo

---
+++ ex02.js
@@ -0,0 +1,8 @@
+function intoTheClub(value){
+    if (value < 18)
+      return("Site prohibited for minors.");
+    else if(value > 18)
+        return("You can enter");
+      
+  }
+  intoTheClub(19);
