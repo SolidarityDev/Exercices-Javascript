192703385245700e3143c3ed906fa4ca8d74f30b
Seraphin <aribi.rachid@hotmail.fr>
2020-08-10 12:05:18 +0200

    six exo

---
+++ ex06.js
@@ -0,0 +1,19 @@
+function checkTheValue(value){
+
+
+    switch (value) {
+        case 3 :
+            console.log("Les trois frères");
+            break;
+        case 6 :
+            console.log("Sixieme sens");
+            break;
+        case 0 :
+            console.log("L'homme invisible");
+            break;
+        default :
+            console.log("Je ne connais pas.");
+            break;
+    }
+    }
+    checkTheValue(0);
